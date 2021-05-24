e81acb86b5cac85909c68b6ec10cfbf40957cddb
Fernando <isquierdo.ads@gmail.com>
2021-05-24 20:34:01 -0300

    Final Version

--- knexfile.js
+++ knexfile.js
@@ -3,10 +3,10 @@ module.exports = {
   development: {
     client: 'mysql2',
     connection: {
-      host : 'localhost',
-      user : 'root',
+      host : 'freedb.tech',
+      user : 'freedbtech_isquierdoads',
       password : '12345',
-      database : 'futebolavenida'
+      database : 'freedbtech_futebolavenida'
     },
     migrations: {
       tableName: 'migrations',
