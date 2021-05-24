5c4882bea7e54ce38fe29fb6b345d4613be3ab59
Fernando <isquierdo.ads@gmail.com>
2021-05-24 20:58:25 -0300

    Final Version

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3306
 const routes = require('./routes')
 app.use(express.json());
 app.use(routes);
