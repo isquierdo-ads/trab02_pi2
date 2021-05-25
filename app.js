c03e87e32068bd7185ea66ff24ef3368152e0e62
Fernando <isquierdo.ads@gmail.com>
2021-05-25 19:45:47 -0300

    Final Version

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
-const express = require('express')
-const app = express()
-const port = process.env.PORT || 3306
-const routes = require('./routes')
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3306;
+const routes = require("./routes");
 app.use(express.json());
 app.use(routes);
 
-app.get('/', (req, res) => {
-  res.send('<h1>Futebol Avenida!<h1>')
-})
+app.get("/", (req, res) => {
+  res.send('<p style="color: blue; font-size: 46px;> FUTEBOL AVENIDA </p>');
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+  console.log(`Example app listening at http://localhost:${port}`);
+});
